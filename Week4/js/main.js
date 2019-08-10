d5d1d4aef67d80dc0de7317b7ede9d3af23e319e
Eric Poitras <EricPoitras@users.noreply.github.com>
2019-08-09 21:43:44 -0600

    Add files via upload

--- Week4/js/main.js
+++ Week4/js/main.js
@@ -1,3 +1,16 @@
+var cont_sidebar = document.getElementById("cont_sidebar");
+var btn_settings = document.getElementById("btn_settings");
+
+console.log(cont_sidebar);
+console.log(btn_settings);
+
+btn_settings.addEventListener('click',function(){
+    cont_sidebar.classList.toggle("active");
+});
+cont_sidebar.addEventListener('dblclick',function(){
+    cont_sidebar.classList.toggle("active");
+});
+
 var chart = new ApexCharts(
     document.querySelector("#chart0"),
     options.data.items[0]
